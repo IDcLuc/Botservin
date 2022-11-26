9824a362a2451cbdf92f06ae7ec92b96f52edac3
IDcLuc <lucativio@example.com>
2022-11-26 14:49:44 +0100

    use try catch for run

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'interactionCreate',
     run: async ({client}, interaction) => {
-        console.log(interaction)
 
         const slashcmd = client.commands.get(interaction.commandName)
         if (!slashcmd) return interaction.reply('Invalid slash comamnd')
@@ -9,6 +8,10 @@ module.exports = {
         if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
             return interaction.editReply('You don\'t have the required permissions to run this command.')
 
-        slashcmd.run({client, interaction})
+        try {
+            slashcmd.run({client, interaction})
+        } catch(err) {
+            console.log(err)
+        }
     }
 }
