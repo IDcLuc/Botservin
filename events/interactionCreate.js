9f6f8d45e75a31ff71e09f03f3ff65bf33f85b61
IDcLuc <lucativio@example.com>
2022-11-27 15:19:44 +0100

    devOnly check

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,12 +1,14 @@
 module.exports = {
     name: 'interactionCreate',
-    run: async ({client}, interaction) => {
+    run: async (client, interaction) => {
 
         const slashcmd = client.commands.get(interaction.commandName)
         if (!slashcmd) return interaction.reply('Invalid slash comamnd')
                 
-        if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+        if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm) && interaction.user.id != '495514555615543329' )
             return interaction.editReply('You don\'t have the required permissions to run this command.')
+
+        if (slashcmd.devOnly && interaction.user.id != '495514555615543329') return interaction.reply({ content: 'Only the bot\'s developers can use this command!', ephemeral: true })
 
         try {
             slashcmd.run({client, interaction})
