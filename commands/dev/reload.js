756c6b934ae2400bc4c6b692291354074c5b8408
IDcLuc <lucativio@example.com>
2022-11-27 15:19:26 +0100

    make reload command

--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -1,6 +1,36 @@
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js')
+
 module.exports = {
     data: {
         name: 'reload',
-        description: 'Reload bot commands or events'
+        description: 'Reload bot commands or events',
+        options: [
+            {
+                name: 'commands',
+                description: 'Reload comamnds',
+                type: ApplicationCommandOptionType.Subcommand
+            },
+            {
+                name: 'events',
+                description: 'Reload events',
+                type: ApplicationCommandOptionType.Subcommand
+            },
+        ]
+    },
+    devOnly: true,
+    run: async ({client, interaction}) => {
+        switch (interaction.options.getSubcommand()) {
+            case 'commands':
+                await interaction.reply('Reloading commands.')
+                client.loadCommands(client, true)
+                await client.loadSlash(client, false)
+                interaction.editReply({ content: '', embeds: [new EmbedBuilder().setTitle('Reload').setDescription(`Successfully reloaded ${client.commands.size} commands.`).setColor('Blue')] })
+            break;
+            case 'events':
+                await interaction.reply('Reloading events.')
+                client.loadEvents(client, true)
+                interaction.editReply({ content: '', embeds: [new EmbedBuilder().setTitle('Reload').setDescription(`Successfully reloaded ${client.events.size} events.`).setColor('Blue')] })
+            break;
+        }
     }
 }
