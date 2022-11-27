5d15fa5315fdcae8c1f9ad94f16e772679517bc1
IDcLuc <lucativio@example.com>
2022-11-27 15:18:56 +0100

    make slash cmds handler

---
+++ handlers/slashcommands.js
@@ -0,0 +1,25 @@
+const { REST, Routes } = require('discord.js')
+
+module.exports = async (client, exit) => {
+    client.loadCommands(client, true)
+
+    if (!reload) console.log(`Refreshing ${client.commands.size} slash commands`);
+
+    const commands = []
+    client.commands.forEach(command => {
+        commands.push(command.data)
+    });
+
+    const rest = new REST({ version: 10 }).setToken(process.env.token)
+
+    await rest.put(
+        Routes.applicationCommands('1002272295143878758'),
+        { body: commands }
+    ).then(data => {
+        if (!reload) console.log(`Successfully loaded ${data.length || 0} slash commands.`)
+        if (exit) process.exit(0)
+    }).catch(err => {
+        console.log(err)
+        process.exit(1)
+    })
+}
